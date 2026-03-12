1c52858a14685387cc7817f756fb3c0ce932a2c1
Dormamos64 <gabrieloliveiraamaral06@gmail.com>
2026-03-12 20:42:40 -0300

    second commit

--- estatistica.py
+++ estatistica.py
@@ -11,3 +11,31 @@ df = pd.read_excel(url, engine='openpyxl')
 df.head()
 
 print(df.head)
+
+'''qualitativa nominal = coisas q n da para colocar em ordem, ierarquia ex. genero, cidade, cor.
+-grupo
+-descrição
+-insumo
+-nome_obra
+-id_ccoi_elemento
+-id_appropriation_compostition
+-ip_d
+-ip_acum
+
+
+qualitativa ordinal = é oq da para ordenar sem ser numero ex. patentes , classificação.
+-codigo_cc 
+-unid
+
+
+
+quantitativa discreta = quando c consegue contar sendo valores inteiros e finitos ex. numeros 1,2,3...
+-elemento
+
+
+quantitativa contínua = quando n da para contar ex. peso exato altura exata etc tempo, temperatura ....
+-app_inicio
+-app_fim
+-qntd
+'''
+
