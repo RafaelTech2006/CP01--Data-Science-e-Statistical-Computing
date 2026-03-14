778236cf94777ef0ef97c86e30c76d9557c1afb4
RafaelTech2006 <rafatavaresstos@gmail.com>
2026-03-14 07:34:53 -0300

    código atualizado

--- estatistica.py
+++ estatistica.py
@@ -1,48 +1,40 @@
 import pandas as pd
 
+#IMPORTAÇÃO E RECONHECIMENTO DA BASE
 
-# URL do arquivo Excel
 url = 'https://github.com/Dormamos64/excell/raw/refs/heads/main/df_diarios.xlsx'
-
-# Ler o arquivo Excel diretamente da URL
 df = pd.read_excel(url, engine='openpyxl')
+print(df.head())
 
-# TIPO DE VARIÁVEL DE COLUNAS PERTINENTES 
+#TIPO DE VARIÁVEL / EXPLICAÇÃO DE CADA COLUNA 
+
 # QUALITATIVA NOMINAL: 
-# classe -> nomeia as funções
-# caderno -> infraestrutura e edificio 
-# grupo -> organiza as funções em grupos        
-# codigo_cc -> identificador
-# descricao -> tarefas
-# codin -> identificador 
-# unidin -> unidade de medida   
-# tipo_insumo -> cclassifica entre mão de obra e equipamento
-# id_ccoi_elemento -> identificador
-# id_appropriation_composition -> identificador
+# classe -> descrição da atividade. Diz o que é feito, sem dizer a função.
+# caderno -> frente. Divide-se entre infraestrutura e edificio.
+# grupo -> descrição da atividade. Diz o que é feito, sem dizer a função.        
+# codigo_cc -> identificação da obra. É uma sequência de numeros e caracteres padronizados, que possuem a função de identificar na obra
+# descricao -> etapa da obra. São objetivos a serem cumpridos
+# codins -> identificação da obra. Sequência de números, que possuem a função de identificar na obra
+# unidins -> unidades de medida usadas na obra
+# tipo_insumo -> tipo insumo. São identificados por mão de obra e equipamento
+# id_ccoi_elemento -> identificação da obra. Sequência de números, que possuem a função de identificar na obra
+# id_appropriation_composition ->  identificação da obra. Sequência de números, que possuem a função de identificar na obra
 
 
 # QUALITATIVA ORDINAL:
-# unid -> unidad de medida
-# insumo -> reccursos utilizados
+# unid -> unidade de medida usadas na obra
+# insumo -> tipo de insumo. Cita a mão de obra e equipamento presentes na obra
 
 # QUANTITATIVA DISCRETA:
-# elemen -> identificador
+# elemen -> identificador da obra
 
 # QUANTITATIVA CONTÍNUA:
-# app_inicio -> data/hora de início
-# app_fim -> data/hora de término
-# qntd -> quantidade
-# qs -> quantidade 
-# data -> data/hora 
+# app_inicio ->período de registro. Informa o dia, mês, ano e horário de início do registro.
+# app_fim -> período de registro. Informa o dia, mês, ano e horário do fim do registro.
+# qntd -> quantidade produzida.
+# qs -> quantidade produzida.
+# data -> período de registro
 # qntd_ac -> quantiade de insumo
-# qs_acu -> 
-# ip_d -> 
-# ip_acu ->
-  
-
-# Exibir as primeiras linhas do DataFrame
-print(df.head())
-
-
-
-
+# qs_acum -> indicadores de produtividade
+# ip_d -> indicador de produtividade
+# ip_acum -> indicador de produtividade
